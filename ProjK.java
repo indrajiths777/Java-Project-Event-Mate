f4e87c20d119f2500011095f88e8c3e186ef11b3
Indrajith S <indrajiths797@gmail.com>
2025-09-19 20:11:02 +0530

    Update ProjK.java

--- ProjK.java
+++ ProjK.java
@@ -3,50 +3,46 @@ package projk;
 import javax.swing.*;
 import java.awt.*;
 
-public class ProjK {
-    public static void main(String[] args) {
+
+public class ProjK extends JFrame {
+
+   
+    public ProjK() {
         
-        JFrame frame = new JFrame("Event Mate"); 
+        setTitle("Event Mate");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+        setSize(450, 300); 
+        setLocationRelativeTo(null);
+        getContentPane().setBackground(new Color(220, 240, 220));
+        setLayout(null); 
 
         
         JLabel titleLabel = new JLabel("Event Mate", SwingConstants.CENTER);
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 28)); 
-        titleLabel.setBounds(0, 20, 450, 30); 
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 28));
+        titleLabel.setBounds(0, 20, 450, 30);
 
-        
         JLabel collegeLabel = new JLabel("College Name:");
-        collegeLabel.setBounds(50, 80, 100, 25); 
+        collegeLabel.setBounds(50, 80, 100, 25);
         JTextField collegeSearchBox = new JTextField(20);
-        collegeSearchBox.setBounds(160, 80, 220, 25); 
+        collegeSearchBox.setBounds(160, 80, 220, 25);
 
-        
         JLabel eventLabel = new JLabel("Event Name:");
-        eventLabel.setBounds(50, 120, 100, 25); 
+        eventLabel.setBounds(50, 120, 100, 25);
         JTextField eventSearchBox = new JTextField(20);
-        eventSearchBox.setBounds(160, 120, 220, 25); 
-        
-        
+        eventSearchBox.setBounds(160, 120, 220, 25);
+
         JButton clickButton = new JButton("Click Here");
-        
-        clickButton.setBounds(175, 160, 100, 30);
-       
-        frame.setLayout(null);
+        clickButton.setBounds(175, 180, 100, 30); 
 
         
-        frame.add(titleLabel);
-        frame.add(collegeLabel);
-        frame.add(collegeSearchBox);
-        frame.add(eventLabel);
-        frame.add(eventSearchBox);
-        frame.add(clickButton); 
-
-        
-        frame.getContentPane().setBackground(new Color(220, 240, 220));
-
-        
-        frame.setSize(450, 250);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null); 
+        add(titleLabel);
+        add(collegeLabel);
+        add(collegeSearchBox);
+        add(eventLabel);
+        add(eventSearchBox);
+        add(clickButton);
     }
+    
+   
 }
+
